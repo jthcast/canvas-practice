a6a99a753f458880211b90d9ba0114a4f3732577
jthcast <jthcast@gmail.com>
2021-02-06 21:59:54 +0900

    Fix remove gui

--- pages/breakout.tsx
+++ pages/breakout.tsx
@@ -180,7 +180,6 @@ export default function Breakout() {
 
   return (
     <div className={styles.container}>
-      <Gui />
       <canvas ref={canvasEl} width={400} height={400} />
       <a
         href={`https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript`}
