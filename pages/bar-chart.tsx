5191101708cf541a20f33be438732ace5b6f1d0a
jthcast <jthcast@gmail.com>
2021-02-12 20:03:29 +0900

    WIP barchart with bar

--- pages/bar-chart.tsx
+++ pages/bar-chart.tsx
@@ -4,6 +4,7 @@ import styles from '../styles/Breakout.module.css'
 interface BarchartOptions {
   canvas: HTMLCanvasElement;
   padding: number;
+  gridColor: string;
   gridScale: number;
   data: Record<string, number>;
   colors: string[];
@@ -45,13 +46,38 @@ export default function BarChart() {
   function drawBarchart(options: BarchartOptions) {
     const canvas = options.canvas;
     const ctx = canvas.getContext('2d');
+    const padding = options.padding;
     const colors = options.colors;
     const data = options.data;
+    const gridColor = options.gridColor;
+    const gridScale = options.gridScale;
 
     function draw() {
-      let maxValue = 0;
-      for (let categ in data) {
-        maxValue = Math.max(maxValue, data[categ]);
+      const maxValue = Math.max.apply(null, Object.values(data)) ?? 0; //drawing the grid lines
+      const canvasActualHeight = canvas.height - padding * 2;
+      const canvasActualWidth = canvas.width - padding * 2;
+      let gridValue = 0;
+
+      while (gridValue <= maxValue) {
+        const gridY = canvasActualHeight * (1 - gridValue / maxValue) + padding;
+        drawLine(ctx, 0, gridY, canvas.width, gridY, gridColor);
+        ctx.save();
+        ctx.fillStyle = gridColor;
+        ctx.font = 'bold 10px Arial';
+        ctx.fillText(gridValue.toString(), 0, gridY - 2);
+        ctx.restore();
+        gridValue += gridScale;
+      }
+
+      let barIndex = 0; //drawing the bars
+      const numberOfBars = Object.keys(data).length;
+      const barSize = canvasActualWidth / numberOfBars;
+
+      for (const categ in data) {
+        const val = data[categ];
+        const barHeight = Math.round(canvasActualHeight * (val / maxValue));
+        drawBar(ctx, padding + (barIndex * barSize), canvas.height - barHeight - padding, barSize, barHeight, colors[barIndex % colors.length]);
+        barIndex++;
       }
     }
     draw();
@@ -61,12 +87,11 @@ export default function BarChart() {
     const canvas = canvasEl.current;
     canvas.width = 300;
     canvas.height = 300;
-    const ctx = canvas.getContext('2d');
     drawBarchart({
       canvas: canvas,
       padding: 10,
       gridScale: 5,
-      // gridColor:"#eeeeee",
+      gridColor: "#e5e5e5",
       data: mockData,
       colors: ["#a55ca5", "#67b6c7", "#bccd7a", "#eb9743"]
     });
